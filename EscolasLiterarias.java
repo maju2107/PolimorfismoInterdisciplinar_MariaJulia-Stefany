80af80a8aa99a78e987e770fa583232e724ec37c
maju2107 <maju.mariajuliaaraujo@gmail.com>
2025-05-12 08:29:24 -0300

    Adicionado o conteudo

---
+++ EscolasLiterarias.java
@@ -0,0 +1,25 @@
+public abstract class EscolasLiterarias{
+    
+    private String nome;
+    private String periodo;
+
+    public void setNome (String nome){
+        this.nome = nome;
+    }
+
+    public void setPeriodo (String perido){
+        this.periodo = perido;
+    }
+
+    public String getNome (){
+        return nome;
+    }
+
+    public String getPerido (){
+        return periodo;
+    }
+
+    public abstract String definicao();
+    public abstract String quiz();
+
+}
